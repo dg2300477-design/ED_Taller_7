173fb60fbbaf593ff5a3e009d8bb4b2247a4bcc5
GIO_77 <dg2300477@gmail.com>
2026-03-16 08:01:17 -0500

    nodo

---
+++ Node.cs
@@ -0,0 +1,14 @@
+﻿namespace ED_Taller_7
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
