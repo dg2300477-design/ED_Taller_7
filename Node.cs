ea7241a5fafea9092cbfbb5c67b9e4add295b90d
nico01625 <164085590+nico01625@users.noreply.github.com>
2026-03-18 20:27:09 -0500

    comentarios Stack Array

--- Node.cs
+++ Node.cs
@@ -2,7 +2,7 @@
 {
     public class Node<T>
     {
-        public T Data { get; set; }
+        public T Data { get; set; }// Guarda y leer
         public Node<T> Next { get; set; }
 
         public Node(T data)
--- StackArray.cs
+++ StackArray.cs
@@ -34,26 +34,27 @@ namespace ED_Taller_7
 
         public T Pop()
         {
-            //E reduce el tamaño , guarda el valor, libera el objeto -Nulling out references
+            //E reduce el tamaño , guarda el valor, Borra la referencia  -Nulling out references
             if (IsEmpty()) throw new InvalidOperationException("La pila está vacía.");
             size--;
             T data = array[size];
             array[size] = default(T);
-            return data;
+            return data; //Devuelve el elemento
         }
 
         public T Peek()
         {
             if (IsEmpty()) throw new InvalidOperationException("La pila está vacía.");
-            return array[size - 1];
+            return array[size - 1]; //Accede al ultimo elemento ingresado
         }
 
-        public bool IsEmpty() => size == 0;
+        public bool IsEmpty() => size == 0;//condicional si no hay elementos
 
-        public int Count() => size;
+        public int Count() => size; //Devuelve el numero actual elementos
 
         public void Clear()
         {
+            //Se crea un nuevo arreglo y se reinicia la pila
             array = new T[capacity];
             size = 0;
         }
