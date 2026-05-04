514df8ec46aec7a59422d743d16f07e60121c0db
GIO_77 <dg2300477@gmail.com>
2026-05-04 08:54:05 -0500

    XDDDDD

---
+++ Tree/NodeTree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ED_Taller_7.Nodes
+{
+    public class NodeTree<T> where T : IComparable<T>
+    {
+        public T Data { get; set; }
+        public NodeTree<T>? Left { get; set; }
+        public NodeTree<T>? Right { get; set; }
+        public int Height { get; set; }
+
+        public NodeTree(T data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+            Height = 1;
+        }
+    }
+}
